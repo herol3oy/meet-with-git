64650c7f1c2c8ff46b6b3e0696d5fd614dadfe1b
herol3oy <hamedsedighi@gmail.com>
2025-05-22 18:07:25 +0200

    add api type

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import { createClient } from '@/utils/supabase/server'
 
-export async function GET(request: Request) {
+export async function GET(request: Request): Promise<Response> {
   const { searchParams, origin } = new URL(request.url)
   
   const code = searchParams.get('code')
